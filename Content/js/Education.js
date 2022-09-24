fa4f0f6396c96d2b5e19cacbac411fb604ea2af2
Bishop <shantogamer40@gmail.com>
2022-09-24 22:41:45 +0600

    animations added

---
+++ Content/js/Education.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#card1').show("slide", { direction: "up" }, 800);
+    $('#card2').show("slide", { direction: "down" }, 800);
+    $('#card3').show("slide", { direction: "up" }, 800);
+
+    $('#card1').mouseenter(function () {
+       $(this).effect("shake", { times: 1 });;
+    });
+    $('#card2').mouseenter(function () {
+        $(this).effect("shake", { times: 1 });;
+    });
+    $('#card3').mouseenter(function () {
+        $(this).effect("shake", { times: 1 });;
+    });
+
+});
--- Content/js/Index.js
+++ Content/js/Index.js
@@ -1,1 +1,5 @@
-﻿
+﻿$(document).ready(function () {
+    $('#bio').show("slide", { direction: "up" }, 800);
+    $('#details').fadeIn(1500);
+    $('#propic').fadeIn(1500);
+});
